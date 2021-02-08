ff2df461e233a59cc6323233665986096e439252
Taras <tbatyuchok@gmail.com>
2021-02-08 21:35:21 +0200

    add form and modal

--- monotexhbud/js/script.js
+++ monotexhbud/js/script.js
@@ -9,17 +9,27 @@ const navSlider = () => {
 
 
 		//Animate links
-		navLinks.forEach((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = "";
-			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5
-					}s`;
-			}
-		});
+		// navLinks.forEach((link, index) => {
+		// 	if (link.style.animation) {
+		// 		link.style.animation = "";
+		// 	} else {
+		// 		link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5
+		// 			}s`;
+		// 	}
+		// });
 		// burger animation
 		burger.classList.toggle('toggle');
 	});
+
+	nav.addEventListener("click", () => {
+		
+		nav.classList.remove("nav-active")
+		burger.classList.remove('toggle');
+
+		
+	});
+
+	
 
 
 };
@@ -40,13 +50,25 @@ window.onload = function () {
 $(document).ready(function () {
 	// $('#consultation-form').validate();
 
-	// $('.button').on('click',function(){
-	//     $('.overlay, #thanks').fadeIn('slow');
-	// });
+
+
 
 	$('.modal__close').on('click', function () {
-		$('.overlay, #thanks').fadeOut('slow');
+		$('.overlay, #thanks, #order' ).fadeOut('slow');
 	})
+	// modal
+	// $('.dws').on('click',function(){
+	// 	$('.overlay, #order').fadeIn('slow');
+	// });
+
+	$('.blocImg').each(function(i){
+		$(this).on('click', function(){
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		});
+	});
+
+	
 
 	function valideForms(form) {
 		$(form).validate({
@@ -65,12 +87,12 @@ $(document).ready(function () {
 			},
 			messages: {
 				name: {
-					required: "Пожалуйста, введите свое имя",
+					required: "Введите свое имя",
 					minlength: jQuery.validator.format("Введите {0} символов!")
 				},
-				phone: "Пожалуста, введите свой номер телефона",
+				phone: "Введите свой номер телефона",
 				email: {
-					required: "Пожалуста, введите свой почту",
+					required: "Введите свою почту",
 					email: "Неправильно введен адрес почты"
 				}
 			}
@@ -78,6 +100,7 @@ $(document).ready(function () {
 	};
 
 	valideForms('#consultation-form');
+	valideForms('#order form');
 
 	$('input[name=phone]').mask("+38(999) 999-99-99")
 
@@ -93,11 +116,105 @@ $(document).ready(function () {
 		}).done(function () {
 			$(this).find("input").val("");
 
+
+			$('.overlay, #order').fadeOut('slow');
 			$('.overlay, #thanks').fadeIn('slow');
 
 			$('form').trigger('reset');
 		});
 		return false;
+	});
+
+// nav
+	$('.slide-section').click(function(e){
+		
+		let linkHref = $(this).attr('href')
+		
+		$('html, body').animate({
+			scrollTop: $(linkHref).offset().top
+		});
+
+
+		e.preventDefault();
+	});
+
+
+	
+
+	var arrLang={
+		'ru':{
+			'home':	'Главная',
+			'about': 'О нас',
+			'services': 'Услуги',
+			'contact': 'Контакты',
+			'about-us': 'Наша компания предлагает качественные услуги по диагностике, ремонту, реконструкции и обслуживания подъемных механизмов.(Индивидуальный подход к каждому клиенту)',
+			'btn': 'консультация',
+			'free-cons': ' Получите бесплатную консультацию по ремонту вашей техники',
+			'simple-form': ` Заполните форму заявки 
+			и мы передзвоним вам в течение 10 мин.`,
+			'order':'Заказать консультацию',
+			'about-monotech':'МоноТеxБуд это',
+			'box_one':`Индивидуальный подход к каждого клиента`,
+			'box_two':'Более 50 успешных проектов',
+			'box_three':'200 единиц отремонтированной техники',
+			'box_four':'Максимально быстрый срок выполнения заказа',
+			'box_five':'Лояльная ценовая политика',
+			'box_six':'Коллектив профессионалов который готов ко всем задачам',
+			'expert':'Предоставляем услуги',
+			'h3-two':'Реставрацыя',
+			'h3-three':'Ремонт мостовых кранов',
+			'h3-four':'Ремонт автокранов',
+			'h3-five':'Ремонт автовышек',
+			'h3-six':'Ремонт козловых кранов',
+			'city':'Львов ул.Бескидська №37',
+			'time':'время роботи',
+			'thanks':'Спасибо за вашу заявку',
+			'meneger':'Наш менеджер свяжеться с вами в ближайшее время!',
+			'interested':'Вас интересует:',
+		},	
+
+		'ua': {
+			'home':	'Головна',
+			'about': 'Про нас',
+			'services': 'Послуги',
+			'contact': 'Контакти',
+			'about-us': 'Наша компанія пропонує якісні послуги з діагностики, ремонту, реконструкції та обслуговування підіймальних механізмів.(Індивідуальний підхід до кожного клієнта)',
+			'btn': 'консультація',
+			'free-cons': 'Отримайте безкоштовну консультацію по ремонту вашої техніки',
+			'simple-form': ` Просто заповніть форму заявки  
+			і ми передзвонимо вам за 10 хв.`,
+			'order':'Замовити консультацію',
+			'about-monotech':'МоноТеxБуд це',
+			'box_one':'Індивідуальний підхід до кожного клієнта',
+			'box_two':'Більше 50 успішних проектів',
+			'box_three':'200 одиниць відремонтованої техніки',
+			'box_four':'Максимально швидкий термін виконаня замовленя',
+			'box_five':'Лояльна цінова політика',
+			'box_six':'Колектив професіоналів який готовий до усіх завдань',
+			'expert':'Надаємо послуги',
+			'h3-two':'Реставрація',
+			'h3-three':'Ремонт мостових кранів',
+			'h3-four':'Ремонт автокранів',
+			'h3-five':'Ремонт автовишок',
+			'h3-six':'Ремонт козлових кранів',
+			'city':'Львів вул.Бескидська №37',
+			'time':'час роботи',
+			'thanks':'Дякую за вашу заявку',
+			'meneger':'Наш менеджер свяжеться з вами в найближчий час!',
+			'interested':'Ваc цікавить:',
+		},
+		
+		
+	}
+
+	$(function(){
+		$('.translate').click(function(){
+			var lang =jQuery(this).attr("id");
+
+			$('.lang').each(function(index, item){
+				$(this).text(arrLang[lang][$(this).attr('key')]);
+			})
+		})
 	});
 
 });
--- monotexhbud/mailer/smart.php
+++ monotexhbud/mailer/smart.php
@@ -8,17 +8,19 @@ $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 
+
+
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
+$mail->Host = 'ssl://smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'tbatyuchok@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'abrakadabra299792458'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'david.mosk.lviv@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'Prydumaisam91'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('tbatyuchok@gmail.com','Monotechbud'); // от кого будет уходить письмо?
+$mail->setFrom('david.mosk.lviv@gmail.com','Monotechbud'); // от кого будет уходить письмо?
 $mail->addAddress('monotechbud@ukr.net');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
